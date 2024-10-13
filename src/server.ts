98f8b362df82bd4a62de4dab7b07bf82ed2a1c0e
Felipe Cristovão <felipecss88@gmail.com>
2024-10-13 11:59:56 -0300

    Implementação do Helmet para segurança do app

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import helmet from 'helmet';
 import authRoutes from './routes/auth';
 
 const app = express();
+
+app.use(helmet());
 app.use(express.json());
 
 // Rotas
